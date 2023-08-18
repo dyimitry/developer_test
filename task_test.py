056597d9f4d8939363a238f6eaad08369e987a96
Дмитрий Шинкарев <dimonshinkariov111@yandex.ru>
2023-08-18 11:25:25 +0300

    This is a new commit for what I originally planned to be amended

--- task_test.py
+++ task_test.py
@@ -1,17 +1,17 @@
 import math
+
 def zastroischik(N, M, X):
-    uchastkov= math.ceil(N / M)
-    uchastkov_int= int(N/M)
+    uchastkov = math.ceil(N / M)
     summa = 0
-    count_N = N
-    for i in range(1,uchastkov+1):
-        if uchastkov_int != uchastkov and i==uchastkov:
-            count_N =int(M* (count_N / M))
-            summa +=count_N*X
+    count_stay = N
+    for i in range(1, uchastkov + 1):
+        if count_stay < M:
+            summa += count_stay * X
             break
-        count_N -= M
-        summa += M*X
+        count_stay -= M
+        summa += M * X
         X += 1000
     print(summa)
 
-zastroischik(8,3,10000)
+
+zastroischik(10, 3, 10000)
